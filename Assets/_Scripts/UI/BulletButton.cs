db053af765f3dcf4a54cd9ea607c3771f1326012
FlagrateAE <flagrateinc@gmail.com>
2025-10-17 14:49:00 +0300

    feat: bullet selection carette

--- Assets/_Scripts/UI/BulletButton.cs
+++ Assets/_Scripts/UI/BulletButton.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 [RequireComponent(typeof(Image), typeof(Button))]
 public class BulletButton : MonoBehaviour
 {
-    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private RectTransform carette;
+    [SerializeField] private float caretteSpeed = 0.2f;
+    [SerializeField] private Vector2 caretteOffset = new(0, -10);
+
+
+    public void Select()
+    {
+        carette.DOMove(((Vector2)transform.position) + caretteOffset, caretteSpeed);
+    }
 }
