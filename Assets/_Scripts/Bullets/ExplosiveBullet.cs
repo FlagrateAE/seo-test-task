ca2f57b09db4b4ee297d5f6287806e51614bbb2a
FlagrateAE <flagrateinc@gmail.com>
2025-10-17 21:09:40 +0300

    feat: explosions

--- Assets/_Scripts/Bullets/ExplosiveBullet.cs
+++ Assets/_Scripts/Bullets/ExplosiveBullet.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class ExplosiveBullet : BaseBullet
 {
     [SerializeField] private float explosionRadius = 5f;
+    [SerializeField] private GameObject explosionPrefab;
+    [SerializeField] private float explosionDuration = 4f;
 
     private const int Max_Hit_Colliders = 10;
     private readonly Collider[] _hitColliders = new Collider[Max_Hit_Colliders];
+    private GameObject _explosionInstance;
 
     public override void OnCollisionEnter(Collision collision)
     {
+        Explode();
+
         Physics.OverlapSphereNonAlloc(
             collision.contacts[0].point,
             explosionRadius,
@@ -20,7 +25,26 @@ public class ExplosiveBullet : BaseBullet
             Hit(hitCollider.gameObject);
         }
         _hitColliders.Initialize();
+    }
 
-        DestroySelf();
+    private void Explode()
+    {
+        gameObject.SetActive(false);
+
+        _explosionInstance = Instantiate(
+            explosionPrefab,
+            transform.position,
+            Quaternion.identity
+        );
+
+        Invoke(nameof(DestroySelf), explosionDuration);
+    }
+
+    private void OnDestroy()
+    {
+        if (_explosionInstance != null)
+        {
+            Destroy(_explosionInstance);
+        }
     }
 }
