4c6060044c186f96f8b1cfcd4ae85f72c534a9c2
FlagrateAE <flagrateinc@gmail.com>
2025-10-19 22:47:25 +0300

    fix: CoroutineManager quitting

--- Assets/_Scripts/Utilities/CoroutineManager.cs
+++ Assets/_Scripts/Utilities/CoroutineManager.cs
@@ -2,22 +2,30 @@ using UnityEngine;
 using System.Collections;
 using System;
 
+[DefaultExecutionOrder(int.MinValue)]
 public class CoroutineManager : MonoBehaviour
 {
     private static CoroutineManager _instance;
+    private static bool _isQuitting = false;
 
     public static CoroutineManager Instance
     {
         get
         {
+            if (_isQuitting)
+            {
+                return null;
+            }
+
             if (_instance == null)
             {
                 _instance = FindObjectOfType<CoroutineManager>();
-            }
-            if (_instance == null)
-            {
-                var singletonObject = new GameObject(nameof(CoroutineManager));
-                _instance = singletonObject.AddComponent<CoroutineManager>();
+
+                if (_instance == null)
+                {
+                    var singletonObject = new GameObject(nameof(CoroutineManager));
+                    _instance = singletonObject.AddComponent<CoroutineManager>();
+                }
             }
             return _instance;
         }
@@ -34,19 +42,31 @@ public class CoroutineManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    private void OnDestroy()
+    {
+        _isQuitting = true;
+    }
+
     public static void InvokeLater(Action action, float delay)
     {
-        Instance.StartCoroutine(InvokeRoutine(action, delay));
+        if (Instance != null)
+        {
+            Instance.StartCoroutine(InvokeRoutine(action, delay));
+        }
     }
 
     public static Coroutine InvokeLaterCancellable(Action action, float delay)
     {
-        return Instance.StartCoroutine(InvokeRoutine(action, delay));
+        if (Instance != null)
+        {
+            return Instance.StartCoroutine(InvokeRoutine(action, delay));
+        }
+        return null;
     }
 
     public static void Cancel(Coroutine coroutineToCancel)
     {
-        if (coroutineToCancel != null && Instance != null)
+        if (_instance != null && coroutineToCancel != null)
         {
             Instance.StopCoroutine(coroutineToCancel);
         }
